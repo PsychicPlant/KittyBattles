ae0a079dac4c39fe32a1706da398d2112df9bbd9
root <guilhermeruas20@hotmail.com>
2022-11-21 22:04:10 -0500

    lab 9 tasks

--- bestpage1.php
+++ bestpage1.php
@@ -1,6 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
+	<script>
+		function popimage(z)
+		{
+			z.src = "cuh.jpg";
+		}
+	</script>
         <title>My best page</title>
     </head>
     <body>
@@ -9,9 +15,10 @@
         <h3>Get</h3>
         <h2>Much</h2>
         <h1>Higher</h1>
-
+	</br>
+	</br>
         <p>
-            <img src="polygang.png" alt="The meanest frogs in town.">
+            <img onclick="popimage(this)" style="display: block" src="polygang.png" alt="The meanest frogs in town.">
 	    <?php echo "<p/><p>Something in the water.</p>" ?>
         </p>
         <div>
