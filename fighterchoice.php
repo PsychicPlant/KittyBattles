0723021dd5409f1add585f8bd9eeecfd6cef2cf7
PsychicPlant <guilhermeruas20@hotmail.com>
2022-12-21 11:49:55 -0500

    All the SQL functions are working just fine. Must add fighting ring now.

--- fighterchoice.php
+++ fighterchoice.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
+<head
+</head>
 <html>
 <body>
 	<?php
 		$select = htmlspecialchars($_POST["select"]);
+		$fighter_del = htmlspecialchars($_POST["fighters"]);
 		if($select == "1")
-		{ echo "<script>window.open('formpage1.html','_self');</script>";}
-		else{}
+		{ echo "<script>window.open('formpage1.html','_self');</script>"; }
+		else
+		{ echo "<script>window.open('deletedb.php/?id=$fighter_del','_self');</script>"; }
 	?>
 </body>
 </html>
